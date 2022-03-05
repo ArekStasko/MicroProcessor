6a6e06a3948d43f9ce91c82e124a0b646592775f
ArekStasko <arek.stasko@gmail.com>
2022-03-05 16:53:57 +0100

    Add project files.

---
+++ MicroProcessor/MicroController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroProcessor
+{
+    public class MicroController
+    {
+        public string AL { get; set; }
+        public string AH { get; set; }
+        public string BL { get; set; }
+        public string BH { get; set; }
+        public string CL { get; set; }
+        public string CH { get; set; }
+        public string DL { get; set; }
+        public string DM { get; set; }
+
+        public MicroController(List<string> recordsData)
+        {
+            AL = recordsData[0];
+            AH = recordsData[1];
+            BL = recordsData[2];
+            BH = recordsData[3];
+            CL = recordsData[4];
+            CH = recordsData[5];  
+            DL = recordsData[6];
+            DM = recordsData[7];
+        }
+
+        public void PrintRecords()
+        {
+            Console.WriteLine($"AL : {AL}; AH : {AH}; BL: {BL}; BH : {BH}; CL : {CL}; CH: {CH}; DL: {DL}; DM : {DM}");
+        }
+
+    }
+}
---
+++ MicroProcessor/Program.cs
@@ -0,0 +1,38 @@
+﻿using MicroProcessor;
+using System.Globalization;
+
+namespace MicroProcessor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dataToCollect = { "AL", "AH", "BL", "BH", "CL", "CH", "DL", "DM" };
+            var records = new List<string> { };
+
+            foreach (var data in dataToCollect)
+            {
+                Console.WriteLine($"Provide record data for {data}");
+                string providedData = Console.ReadLine();
+
+                while (!CheckCorrection(providedData))
+                {
+                    Console.WriteLine($"You provided wrong data for {data}");
+                    providedData = Console.ReadLine();
+                }
+                records.Add(providedData.ToUpper());
+            }
+
+            var microController = new MicroController(records);
+            microController.PrintRecords();
+        }
+
+        public static bool CheckCorrection(string valToCheck)
+        {
+            if (valToCheck.Length != 2) return false;
+            if (!Int32.TryParse(valToCheck, NumberStyles.HexNumber, null, out int n)) return false;
+
+            return true;
+        }
+    }
+}
